caa7be5c4490de1071c920da6febf64ee67b9c42
MrBlackGhost <hemantkumar4213@icloud.com>
2025-05-05 16:20:15 +0530

    try to work on mem0 with pdf

--- api.py
+++ api.py
@@ -1,204 +1,67 @@
-import json
-import os
-
-import requests
-from fastapi import FastAPI, Body
-from dotenv import load_dotenv
 from openai import OpenAI
+from dotenv import load_dotenv
+import os
+from langsmith.wrappers import wrap_openai
 
 load_dotenv()
 
-api_key = os.getenv("GEMINI_API_KEY")
-print("APIKEY", api_key)
+key = os.getenv("GEMINI_API_KEY")
 
-client = OpenAI(
-    api_key=api_key,
-    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+client = wrap_openai(
+    OpenAI(
+        api_key=key,
+        base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+    )
 )
 
-
-app = FastAPI()
-
-
-def get_weather(place_name: str) -> str:
-    try:
-        result = requests.get(f"https://wttr.in/{place_name}?format=%C+%t", timeout=5)
-        result.raise_for_status()
-        return result.text.strip()
-    except requests.RequestException as e:
-        return f"Error fetching weather: {e}"
-
-
-def run_command(command):
-    print("GOT THE COMMANDS", command)
-    result = os.system(command=command)
-    print("RESULT IN THE RUN_COMMAND", result)
-    return result
-
-
-def check_file(file_name):
-    print("INSIDE THE CHECK_FILE")
-    result = os.path.exists(file_name)
-    print("RESULT IN THE CHECK_FILE", result)
-    if not result:
-        return f"the {file_name} is not created do make the command and run it again"
-
-    return f"the {file_name} is created successfully"
-
-
-available_tools = {
-    "get_weather": {
-        "fn": get_weather,
-        "description": "call a api and get the temp of the request query",
-    },
-    "run_command": {
-        "fn": run_command,
-        "description": "Run a shell command in the current directory",
-    },
-    "check_file": {
-        "fn": check_file,
-        "description": "Check if a file/folder exists in the current directory",
-    },
-}
-
 system_prompt = """
-You are a expert in creating coding project from scratch Coding assistant for the user and which make create file/folder and write  code inside created file according to the user query append code in the file created
-You operate in a loop of start -> plan -> action -> obeserve -> output
-For the given user query, reason step by step and use the available tools as needed.
+You are an Ai assistant which help to guide which LLM to pick for the given query for the task given by the user you pick the best LLM which is best for the job. 
+choose from given LLM GPT-4, Claude 3.7 Sonnet (Anthropic), Gemini, Grok-3 (xAI), DeepSeek-R1 (DeepSeek).
 
-You are capable of running command in shell and editings files
+GPT-4 - Its good in understanding human-like text and good in creative writing, technical explanations
 
-Rules:
-    - Always respond with one JSON Object per step.
-    - Wait for the observation after an Action before proceding.
-    - Use the availabe tool fully to complete the task.
-    - Command should be valid not just some random string should be run in terminal also do not take the command from the example
-    - Always used to go current directory and then execute the command
-    - Validate the input before executing the commands
+Claude 3.7 Sonnet (Anthropic) - Strengths: Constitutional AI principles, "extended thinking mode" for iterative refinement, and accuracy in coding/web development.
+Use Cases: Business automation, sensitive data handling, and ethical AI deployment
 
-Available Tools:
-    - get_weather(place_name): Give the back the Weather details
-    - run_command(command): Run the command in the terminals
-    - check_file(file_name): Check the File/Folder is created inthe current directoryu or not
-    
-Output JSON fromat:
+Gemini - Its good for the scientific research and large pdf and and good in solving  problem-solvind and data analysis
+
+Grok-3 (xAI)- 
+Strengths: Real-time internet scanning ("DeepSearch"), humor-infused interactions, and advanced problem decomposition.
+Use Cases: News analysis, coding assistance, and dynamic customer support
+
+DeepSeek-R1 (DeepSeek)-
+Strengths: Specializes in mathematical reasoning, logical inference, and coding. Uses reinforcement learning for self-verification and cost efficiency (30x cheaper than competitors).
+Use Cases: STEM research, genomic data analysis, and enterprise RAG integration
+
+
+RULES 
+Strictly Answer in JSON fromat only
+Given LLM is best in solving the user query
+OUTPUT 
 {
-    "step": "plan" | "action" | "observe" | "output",
-    "content" :"string",
-    "function": "name of the function (only for the action step),
-    "command":"input for the function (only for action step)
+    "query" : "string"
+    "message": "its a message which tells why pick this LLM"
+    "result": "Name of the LLM"
 }
 
-Example : What is the temp of Newyork
-{"step": "plan", "content": "I need to find the Temp of the place name Newyork"}
-{"step":"action", "function":"get_weather", "place_name":"Newyork" }
-{"step":"action", "function":"run_command", "command":"ls" }
-{"step":"observer", "content": "26 degree C"}
-{"step":"output", "content": "The Temperaterin nework is with moderate it Temp is 26 degree celcius"}
-
-Example 1: create nextjs app with typescript tailwindcss
-
-{"step": "plan", "content": "For creating the nextjs app with typescript and tailwindcss, I will run the following command: npx create-next-app@latest --ts --tailwindcss my-app"}
-{"step":"action", "function":"run_command", "command":"npx create-next-app@latest --ts --tailwindcss my-app" }
-{"step": "plan", "content": "Created the Foleder user ask name Frontend and now i should create a file name Hello.js that user ask"}
-{"step":"action", "function":"run_command", "command":"cd Frontend && touch Hello.js" }
-{"step":"observer", "content": "Check file is created successfully as per the user Requested if yes then go to the next step if not go back to the action step do the left thing"}
-{"step":"output", "content": "As per your req the file is created"}
-
-Example 2: create login page and use tailwindcss for styling
-{"step": "plan", "content": "For creating a login page and use tailwindcss for styling, I will create a new file called login.js in the src/app/login directory and add the following code to it."}
-{"step":"action", "function":"run_command", "command":"touch src/app/login/login.js" }
-{"step":"observer", "content": "I have created a login page and use tailwindcss for styling."}
-{"step":"output", "content": "As per your req the file is created"}
-
-Example 3: create folder name backend and inside it create a  crud node backend
-{"step": "plan", "content": "Ok i have to create a folder name backend into the current directory and inside it i have to create revalent folders"}
-{"step":"action", "function":"run_command", "command":"mkdir backend && cd backend && mkdir models && mkdir routes && mkdir controllers && touch server.js && touch models/userModel.js && touch routes/userRoutes.js && touch controllers/userController.js" }
-{"step":"observer", "content": "Create a Backend folder and put the models routed, controler successfully"}
-{"step":"output", "content": "As per your req the file is created"}
-
-STRICTLY FOLLOW THE BELOW INSTRUCTIONS
-- always use the below code structure for creating nodejs, express, vite applications
-- always modify dependent files also
+Example: 
+{
+    "query": "which llm is good for the coding"
+    "message" "For the Coding basic things like add and multiply fun use DeepSeek-R1 (DeepSeek) for the big project use the Claude 3.7 Sonnet (Anthropic)"
+    "result": "Claude 3.7 Sonnet (Anthropic)"
+}
 """
 
-message = [{"role": "system", "content": system_prompt}]
+messages = [{"role": "system", "content": system_prompt}]
 
+while True:
+    query = input("How may I assist you today?: ")
+    messages.append({"role": "user", "content": query})
 
-@app.post("/")
-def home(input: str = Body(..., description="Chat Message")):
+    res = client.chat.completions.create(
+        model="gemini-2.0-flash",
+        response_format={"type": "json_object"},
+        messages=messages,
+    )
 
-    print("INPUT", input)
-    message.append({"role": "user", "content": input})
-    while True:
-        response = client.chat.completions.create(
-            model="gemini-2.0-flash",
-            messages=message,
-            response_format={"type": "json_object"},
-        )
-
-        print(response.choices[0].message.content)
-
-        parsed_output = json.loads(response.choices[0].message.content)
-
-        message.append({"role": "assistant", "content": json.dumps(parsed_output)})
-
-        step = parsed_output.get("step")
-
-        if step == "plan":
-            print(f"🧐: {parsed_output.get('content')}")
-            continue
-
-        if step == "action":
-            tool_name = parsed_output.get("function")
-            tool_input = parsed_output.get("place_name")
-            comand_input = parsed_output.get("command")
-            file_input = parsed_output.get("file_name")
-
-            if isinstance(tool_input, str):
-                try:
-                    tool_input = json.load(tool_input)
-                    print("TOOL INPUT", tool_input)
-                except:
-                    pass
-            if tool_name in available_tools:
-                print("BEFORE THE API")
-                if tool_name == "get_weather":
-                    result = available_tools[tool_name]["fn"](tool_input)
-                    print("RESULT FORM API", result)
-                    message.append(
-                        {
-                            "role": "assistant",
-                            "content": json.dumps(
-                                {"step": "observe", "content": result}
-                            ),
-                        }
-                    )
-
-                if tool_name == "run_command":
-                    result = available_tools[tool_name]["fn"](comand_input)
-                    message.append(
-                        {
-                            "role": "assistant",
-                            "content": json.dumps(
-                                {"step": "observe", "content": result}
-                            ),
-                        }
-                    )
-                    continue
-                if tool_name == "check_file":
-                    print("BEFORE CALLIND THE CHECK_FILE")
-                    result = available_tools[tool_name]["fn"](file_input)
-                    message.append(
-                        {
-                            "role": "assistant",
-                            "content": json.dumps(
-                                {"step": "observe", "content": result}
-                            ),
-                        }
-                    )
-                    continue
-
-        if step == "output":
-            print(f"🤖: {parsed_output.get('content')}")
-            return parsed_output.get("content")
+    print("------------RESULT-----------", "/n", res.choices[0].message.content)
